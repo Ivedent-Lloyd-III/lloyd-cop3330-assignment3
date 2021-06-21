a9599549e98c679d427824add20699bbffbb109d
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-20 21:02:29 -0400

    Initial GitHub push

--- src/main/java/oop/exercises/ex46/solution46.java
+++ src/main/java/oop/exercises/ex46/solution46.java
@@ -47,4 +47,3 @@ public class solution46 {
         }
     }
 }
-
--- src/test/java/oop/exercises/ex46/solution46Test.java
+++ src/test/java/oop/exercises/ex46/solution46Test.java
@@ -11,7 +11,31 @@ import java.util.Scanner;
 public class solution46Test extends TestCase {
 
     public void test_for_correct_counting() throws FileNotFoundException {
+        String inputString;
+        int count;
+        File file = new File("C:\\Users\\ivede\\Desktop\\OOP Exercises\\exercise46_input.txt");
+        Scanner sc = new Scanner(file);
+        HashMap<String, Integer> map = new HashMap<>();
 
+        while (sc.hasNextLine()) {
+            inputString = sc.nextLine();
+            String[] wordsArray = inputString.split("\\s+");
 
+            for (String word : wordsArray) {
+                if (map.containsKey(word)) {
+                    count = map.get(word);
+                    map.put(word, count + 1);
+                } else {
+                    map.put(word, 1);
+                }
+            }
+        }
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.print(entry.getKey() + " : ");
+            for (int i = 0; i < entry.getValue(); i++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 }
