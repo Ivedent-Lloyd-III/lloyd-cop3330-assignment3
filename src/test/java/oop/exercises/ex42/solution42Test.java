1cf19a4d6d39ebc3a3a8806b42c97159813ade1d
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-06-20 23:53:45 -0400

    GitHub Update

--- src/test/java/oop/exercises/ex42/solution42Test.java
+++ src/test/java/oop/exercises/ex42/solution42Test.java
@@ -2,13 +2,24 @@ package oop.exercises.ex42;
 
 import junit.framework.TestCase;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class solution42Test extends TestCase {
 
-    public void testMain() {
+    public void test_parse_file() throws FileNotFoundException {
 
 
+        File inputFile = new File("C:\\Users\\ivede\\Desktop\\OOP Exercises\\exercise42_input.txt");
+        Scanner scan = new Scanner(inputFile);
 
+        // scan in the string
+        String input = scan.nextLine();
+        String[] output = input.split(",");
 
+        for (String s : output) {
 
+        }
     }
 }
